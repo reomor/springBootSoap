1dddc98eb01038bbd8ff642009b88d6d54806651
Reomor <reomor@gmail.com>
2019-10-12 14:28:37 +0300

    SOAP-1 base soap via spring boot

---
+++ src/main/java/ru/github/reomor/soap/endpoint/CountryEndpoint.java
@@ -0,0 +1,32 @@
+package ru.github.reomor.soap.endpoint;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+import ru.github.reomor.soap.config.WebServiceConfig;
+import ru.github.reomor.soap.model.GetCountryRequest;
+import ru.github.reomor.soap.model.GetCountryResponse;
+import ru.github.reomor.soap.repository.CountryRepository;
+
+@Endpoint
+public class CountryEndpoint {
+    private static final String NAMESPACE_URI = WebServiceConfig.NAMESPACE_URI;
+
+    private CountryRepository countryRepository;
+
+    @Autowired
+    public CountryEndpoint(CountryRepository countryRepository) {
+        this.countryRepository = countryRepository;
+    }
+
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "getCountryRequest")
+    @ResponsePayload
+    public GetCountryResponse getCountry(@RequestPayload GetCountryRequest request) {
+        GetCountryResponse response = new GetCountryResponse();
+        response.setCountry(countryRepository.findCountry(request.getName()));
+
+        return response;
+    }
+}
---
+++ src/test/java/ru/github/reomor/soap/SoapApplicationTests.java
@@ -0,0 +1,17 @@
+package ru.github.reomor.soap;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SoapApplicationTests {
+    /**
+     * Test context load.
+     */
+    @Test
+    public void contextLoads() {
+    }
+}
